7f1828a3ff8bea5a2fca9a1c5273e4e605594d7a
Theawesometurtl <shablammmo@gmail.com>
2022-11-12 13:25:31 -0800

    I made something that should work but idk i guess the answer isn't right

---
+++ LychrelNums.py
@@ -0,0 +1,19 @@
+
+def IsPalendrome(num):
+    backwards = str(num) [::-1]
+    if backwards == str(num):
+        return True
+    return False
+
+
+
+amount=0
+for i in range(1, 10000):
+    num=i
+    amount+=1
+    for x in range(0, 50):
+        num+= int(str(num) [::-1])
+        if IsPalendrome(num):
+            amount-=1
+            print(i, amount, num)
+            break
