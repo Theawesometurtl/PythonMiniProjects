7a6d898b463b2ecc388eb0d57ed28800e4bf2f35
Theawesometurtl <shablammmo@gmail.com>
2022-11-11 18:50:24 -0800

    solved it, nice!

--- maxPathSum.py
+++ maxPathSum.py
@@ -1,7 +1,7 @@
 listOfRows = []
 rows=largestNum=c=r=0
 
-with open('TriangleOfNums1.txt', 'r') as reader:
+with open('TriangleOfNums2.txt', 'r') as reader:
     for line in reader:
         listOfRows.append(line)
         rows+=1
@@ -10,7 +10,6 @@ for i in range(0, len(listOfRows)):
     row = listOfRows[i].split("\n")
     listOfRows[i] = row[0].split()
 
-#brute force
 max = [int(listOfRows[0][0])]
 
 for r in range(1, rows):
@@ -21,8 +20,15 @@ for r in range(1, rows):
             newMax.append(max[c] + int(listOfRows[r][c]))
         else:
             newMax.append(max[c-1] + int(listOfRows[r][c]))
-    print(max[c])
-    print(listOfRows[r][c+1])
     newMax.append(max[c] + int(listOfRows[r][c+1]))
     max = newMax
-    print(newMax)
+print(newMax)
+
+answer = newMax[0];     
+for i in range(1, len(newMax)):        
+   if(newMax[i] > answer):    
+       answer = newMax[i];    
+           
+print("Largest route:", answer)
+#after looking at other's answers, it seems it would've been easier to start
+# at the bottom
