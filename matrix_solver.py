f6e18e4b40b178da4877dd05656a53d76a1ec698
Theawesometurtl <95330177+Theawesometurtl@users.noreply.github.com>
2024-05-15 14:36:39 -0700

    Kian helped solve the case where the matrix has so many zeros that the row mover algorithm maxes out the recursion depth

--- matrix_solver.py
+++ matrix_solver.py
@@ -1,9 +1,24 @@
-matrix = [
-    [2, 0, -3, 4],
-    [1, 5, 3, 2],
-    [-3, 1, 0, 5]
-]
+from math import sin, radians, cos
 
+t1 = 45
+t2 = 90 - t1
+t3 = 20
+t4 = radians(t2 - t3)
+t2 = radians(t2)
+L = 100
+
+matrix = [
+        [sin(t2),sin(t2),0,0,0,0,0,0,0,0,L],
+        [cos(t2),-cos(t2),0,0,0,0,0,0,0,0,0],
+        [-sin(t2),0,sin(t4),sin(t2),cos(t4),0,0,0,0,0,0],
+        [-cos(t2),0,cos(t4),cos(t2),sin(t4),0,0,0,0,0,0],
+        [0,0,-sin(t4),0,0,0,0,1,0,0,0],
+        [0,0,-cos(t4),0,0,1,0,0,0,0,0],
+        [0,0,0,-sin(t2),0,0,-1,0,0,sin(t2),0],
+        [0,0,0,-cos(t2),0,-1,0,0,0,cos(t2),0],
+        [0,0,0,0,-sin(t4),0,0,0,1,0,0],
+        [0,0,0,0,-cos(t4),0,1,0,0,0,0],
+]
 def equalize_columns(c: int, matrix: list[list[int]]) -> list[list[int]]:
     for r, row in enumerate(matrix):
             if row[c] != 0: 
@@ -33,7 +48,24 @@ def make_zeros_convenient(c: int, matrix: list[list[int]], permutations: int = 0
     
 
 def solve_matrix(matrix: list[list[int]]) -> list[list[int]]:
-    matrix = make_zeros_convenient(0, matrix)
+    # matrix = make_zeros_convenient(0, matrix)
+    r=0
+    previously_swapped_r = len(matrix)+1
+    while True:
+        if matrix[r][r] == 0:
+            for i, row in enumerate(matrix):
+                if row[r] != 0 and previously_swapped_r != i:
+                    row_save = matrix[r]
+                    matrix[r] = matrix[i]
+                    matrix[i] = row_save
+                    break
+            previously_swapped_r = r
+            r=0
+        elif r+1 >= len(matrix):
+            break
+        else: r+=1
+
+
 
     for c in range(len(matrix[0])-1):
         # print(matrix)
